e34459e767d48e951d07c2bbfd7f7d73e60fca02
bernhardkern <kern-bernhard@gmx.de>
2015-09-19 00:46:10 +0200

    gitignore

---
+++ src/main/java/de/kern/springbootstarter/SpringbootstarterApplication.java
@@ -0,0 +1,16 @@
+package de.kern.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+public class SpringbootstarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootstarterApplication.class, args);
+    }
+}
