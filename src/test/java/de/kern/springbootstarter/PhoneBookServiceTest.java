cd4f6c7efa61b4c9d9355720ee33e7e316aae23d
Bernhard Kern <kern-bernhard@gmx.de>
2017-01-08 19:25:15 +0100

    fix the starter app

--- src/test/java/de/kern/springbootstarter/PhoneBookServiceTest.java
+++ src/test/java/de/kern/springbootstarter/PhoneBookServiceTest.java
@@ -1,25 +1,22 @@
 package de.kern.springbootstarter;
 
-import junit.framework.TestCase;
-import junit.framework.TestResult;
-import org.hamcrest.collection.IsEmptyCollection;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.not;
-import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.hamcrest.collection.IsEmptyCollection.empty;
 
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SpringbootstarterApplication.class)
-public class PhoneBookServiceTest extends TestCase {
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PhoneBookServiceTest {
 
     @Autowired
     private PhoneBookService sut;
